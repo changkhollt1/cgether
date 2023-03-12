18ae2d7d3d110c147e7b2760713c53e63c218a72
it cuoi <nhocype2013@gmail.com>
2023-03-12 15:58:26 +0700

    thay doi 2 12/3

--- cuoi/settings.py
+++ cuoi/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'livereload',
     'django.contrib.staticfiles',
     'POS',
     'home.apps.HomeConfig',
@@ -53,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'cuoi.urls'
